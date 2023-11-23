9ffe15b64b234869c665859af694548bc41affd7
Giovanni <giovanniar@al.insper.edu.br>
2023-11-22 22:43:42 -0300

    Tentativa de envio só dos conteúdos do kit

--- giogio.py
+++ giogio.py
@@ -1,40 +1,20 @@
-from typing import List, Optional
+from typing import Optional
 from fastapi import Depends, FastAPI, HTTPException, Request, Form
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from fastapi.responses import HTMLResponse
 import ssl
 import time
 import paho.mqtt.client as mqtt_client
-from crud import create_kit, get_kit, create_cirurgia, get_cirurgia
+import crud, models, schemas
 from database import SessionLocal, engine
-import schemas
+import json
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# CORS (Cross-Origin Resource Sharing) middleware for handling Cross-Origin requests
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
-
-# MQTT configuration
-mqtt_broker_address = "your_mqtt_broker_address"
-mqtt_port = 1883
-mqtt_topic = "your_mqtt_topic"
-
-def connect() -> mqtt_client.Client:
-    mqtt_client_id = "fastapi-mqtt-client"
-    mqtt_client = mqtt_client.Client(client_id=mqtt_client_id)
-    mqtt_client.connect(mqtt_broker_address, mqtt_port)
-    return mqtt_client
-
-def publish(mqtt_client: mqtt_client.Client, message: str):
-    mqtt_client.publish(mqtt_topic, message)
-
-# Dependency to get the database session
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -42,35 +22,77 @@ def get_db():
     finally:
         db.close()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# MQTT Broker configuration
+broker = "aspvpxjmfalxx-ats.iot.us-east-1.amazonaws.com"
+port = 443
+topic = "gi/mandando/dados"
+client_id = f'Fernando'
+
+ca = "certs/AmazonRootCA1.pem"
+cert = "certs/6f963f6ec45fbc59ebb98cf9df943424944b334aec0a18ce0f2e7f5d256530c9-certificate.pem.crt"
+private = "certs/6f963f6ec45fbc59ebb98cf9df943424944b334aec0a18ce0f2e7f5d256530c9-private.pem.key"
+
+
+def ssl_alpn():
+    try:
+        ssl_context = ssl.create_default_context()
+        ssl_context.set_alpn_protocols(["x-amzn-mqtt-ca"])
+        ssl_context.load_verify_locations(cafile=ca)
+        ssl_context.load_cert_chain(certfile=cert, keyfile=private)
+        return ssl_context
+    except Exception as e:
+        print("exception ssl_alpn()")
+        raise e
+
+def connect():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    ssl_context = ssl_alpn()
+    client.tls_set_context(context=ssl_context)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, message):
+    time.sleep(1)
+    result = client.publish(topic, message, 0)
+    status = result[0]
+    if status == 0:
+        print(f"Sent `{message}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
-@app.post("/add_kit/", response_model=schemas.Kit)
-def create_kit(kit: schemas.KitCreate, db: Session = Depends(get_db)):
-    return create_kit(db=db, kit=kit)
-
-@app.get("/kit/", response_model=List[schemas.Kit])
-def read_kit(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_kit(db, skip=skip, limit=limit)
-
-@app.post("/add_cirurgia/", response_model=schemas.Cirurgia)
-def create_cirurgia(cirurgia: schemas.CirurgiaCreate, db: Session = Depends(get_db)):
-    return create_cirurgia(db=db, cirurgia=cirurgia)
-
-@app.get("/cirurgia/", response_model=List[schemas.Cirurgia])
-def read_cirurgia(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_cirurgia(db, skip=skip, limit=limit)
-
 @app.post("/e/")
 async def process_form(request: Request):
+    #created_kit = crud.create_kit(db=db, kit=kit)
+    #order = crud.get_order(db, created_kit.order_id)
+    #order_items = order.items
+    
     try:
         argumento_envio = await request.json()
-        print(argumento_envio["kit"])
+        print(argumento_envio)
         # Publish user input to MQTT broker
-        mqtt_client_instance = connect()
-        publish(mqtt_client_instance, argumento_envio["kit"])
-        mqtt_client_instance.disconnect()
+        mqtt_client = connect()
+        publish(mqtt_client, argumento_envio)
+        mqtt_client.disconnect()
         return {"status": "success"}
     except Exception as e:
         raise HTTPException(status_code=422, detail=str(e))
